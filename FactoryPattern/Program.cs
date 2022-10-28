2e56120f6acefa4d5be6f23faa3c24eed61dfdda
Jonathan-Wills <wills830@hotmail.com>
2022-10-27 23:47:47 -0400

    initial commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Enter the amount of wheels for the vehicle you wish to create");
+            int wheelCount;
+            var input = int.TryParse(Console.ReadLine(), out wheelCount);
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+            vehicle.Drive();
         }
     }
 }
